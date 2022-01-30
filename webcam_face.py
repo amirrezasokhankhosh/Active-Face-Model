f7dd40b918f23f0401543cc6cffd03e11710344c
SepehrShahsavar <sepehrshahsavar@email.kntu.ac.ir>
2022-01-30 15:24:32 +0330

    task1Functionalized.

--- webcam_face.py
+++ webcam_face.py
@@ -25,14 +25,45 @@ def plot_face(X, color='b'):
         yj = X[j, 1]
         # draw a line between X[i] and X[j]
         plt.plot((xi, xj), (yi, yj), '-', color=color)
-        
+
+
+def register_affine_face(base_face, face):
+    mean = np.mean(face, axis=0)
+    of = face - mean
+    bf = base_face - np.mean(base_face, axis=0)
+    new_face = np.c_[of, np.zeros((68, 2))]
+    new_face = np.repeat(new_face, repeats=2, axis=0)
+    new_face[1::2, [2, 0]] = new_face[1::2, [0, 2]]
+    new_face[1::2, [3, 1]] = new_face[1::2, [1, 3]]
+
+    new_base_face = np.ravel(bf)
+    y = np.linalg.lstsq(new_face, new_base_face, rcond=None)[0]
+    registered_face = new_face @ y
+    return np.reshape(registered_face, (68, 2))
+
+
+def registered_similarity_face(base_face, face):
+    mean = np.mean(face, axis=0)
+    of = face - mean
+    bf = base_face - np.mean(base_face, axis=0)
+    new_face = np.repeat(of, repeats=2, axis=0)
+    new_face[1::2, 0] = new_face[1::2, 0] * -1
+    new_face[1::2, [0, 1]] = new_face[1::2, [1, 0]]
+
+    new_base_face = np.ravel(bf)
+    y = np.linalg.lstsq(new_face, new_base_face, rcond=None)[0]
+    registered_face = new_face @ y
+    return np.reshape(registered_face, (68, 2))
+
+
 def PCA(X, avg, k):
     Z = np.zeros((136, len(face_list)))
     for i in range(len(face_list)):
         Z[:, i] = X[i].reshape((1, 136)) - avg.reshape((1, 136))
     U, sigma, V = np.linalg.svd(Z, full_matrices=False)
-    U, sigma, V = U[:,0:k], sigma[0:k], V[0:k, :]
+    U, sigma, V = U[:, 0:k], sigma[0:k], V[0:k, :]
     return U, sigma, V
+
 
 def animate(U, sigma, avg, k, color='b'):
     for i in range(k):
@@ -44,13 +75,6 @@ def animate(U, sigma, avg, k, color='b'):
             plot_face(A, color)
             plt.draw()
             plt.pause(.5)
-    
-    
-    
-
-
-
-
 
 
 vc = cv2.VideoCapture(0)
@@ -83,7 +107,7 @@ while rval:
         shape = predictor(img, d)
 
         X = np.array([(shape.part(i).x, shape.part(i).y)
-                     for i in range(shape.num_parts)])
+                      for i in range(shape.num_parts)])
         theta = np.radians(180)
         c, s = np.cos(theta), np.sin(theta)
         R = np.array(((c, -s), (s, c)))
@@ -101,26 +125,25 @@ while rval:
         # print('X =', X)
         face_list.append(Z)
 
-
 base_face = face_list[0]
 affine_registered_list = []
 affine_registered_list.append(base_face)
 # averaging faces - affine register
-for i in range(1, len(face_list)):
-    face = face_list[i]
-    mean = np.mean(face, axis=0)
-    of = face - mean
-    bf = base_face - np.mean(base_face, axis=0)
-    new_face = np.c_[of, np.zeros((68, 2))]
-    new_face = np.repeat(new_face, repeats=2, axis=0)
-    new_face[1::2, [2, 0]] = new_face[1::2, [0, 2]]
-    new_face[1::2, [3, 1]] = new_face[1::2, [1, 3]]
-
-    new_base_face = np.ravel(bf)
-    y = np.linalg.lstsq(new_face, new_base_face, rcond=None)[0]
-    registered_face = new_face @ y
-    t = np.reshape(registered_face, (68, 2))
-    affine_registered_list.append(t)
+# for i in range(1, len(face_list)):
+#     face = face_list[i]
+#     mean = np.mean(face, axis=0)
+#     of = face - mean
+#     bf = base_face - np.mean(base_face, axis=0)
+#     new_face = np.c_[of, np.zeros((68, 2))]
+#     new_face = np.repeat(new_face, repeats=2, axis=0)
+#     new_face[1::2, [2, 0]] = new_face[1::2, [0, 2]]
+#     new_face[1::2, [3, 1]] = new_face[1::2, [1, 3]]
+#
+#     new_base_face = np.ravel(bf)
+#     y = np.linalg.lstsq(new_face, new_base_face, rcond=None)[0]
+#     registered_face = new_face @ y
+#     t = np.reshape(registered_face, (68, 2))
+#     affine_registered_list.append(t)
     # plt.plot(t[:, 0], t[:, 1], 'o', color='k', markersize=3)
     # plt.plot(bf[:, 0], bf[:, 1], 'o', color='r', markersize=3)
     # plt.show()
@@ -128,20 +151,20 @@ for i in range(1, len(face_list)):
 # averaging faces - similarity register
 similarity_registered_list = []
 similarity_registered_list.append(base_face)
-for i in range(1, len(face_list)):
-    face = face_list[i]
-    mean = np.mean(face, axis=0)
-    of = face - mean
-    bf = base_face - np.mean(base_face, axis=0)
-    new_face = np.repeat(of, repeats=2, axis=0)
-    new_face[1::2, 0] = new_face[1::2, 0] * -1
-    new_face[1::2, [0, 1]] = new_face[1::2, [1, 0]]
-
-    new_base_face = np.ravel(bf)
-    y = np.linalg.lstsq(new_face, new_base_face, rcond=None)[0]
-    registered_face = new_face @ y
-    t = np.reshape(registered_face, (68, 2))
-    similarity_registered_list.append(t)
+# for i in range(1, len(face_list)):
+#     face = face_list[i]
+#     mean = np.mean(face, axis=0)
+#     of = face - mean
+#     bf = base_face - np.mean(base_face, axis=0)
+#     new_face = np.repeat(of, repeats=2, axis=0)
+#     new_face[1::2, 0] = new_face[1::2, 0] * -1
+#     new_face[1::2, [0, 1]] = new_face[1::2, [1, 0]]
+#
+#     new_base_face = np.ravel(bf)
+#     y = np.linalg.lstsq(new_face, new_base_face, rcond=None)[0]
+#     registered_face = new_face @ y
+#     t = np.reshape(registered_face, (68, 2))
+#     similarity_registered_list.append(t)
     # plt.plot(t[:, 0], t[:, 1], 'o', color='b', markersize=3)
     # plt.plot(bf[:, 0], bf[:, 1], 'o', color='r', markersize=3)
     # plt.show()
@@ -153,7 +176,3 @@ k = len(face_list) - 3
 
 U, sigma, V = PCA(similarity_registered_list, avg, k)
 animate(U, sigma, avg, k)
-
-
-        
-
