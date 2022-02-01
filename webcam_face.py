9ff0074379aa3611615a5ebb745cab73a8a28712
amirreza sokhankhosh <amirrezasokhankhosh79@gmail.com>
2022-02-01 20:04:39 +0330

    Final Commit

--- webcam_face.py
+++ webcam_face.py
@@ -16,8 +16,8 @@ def collect_faces():
     detector = dlib.get_frontal_face_detector()
     predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
 
-    for i in range(1, 14):
-        path = 'D:\\Uni stuff\\LA\\LA-prj\\' + str(i) + '.jpg'
+    for i in range(1, 44):
+        path = 'C:\\Users\\amirr\\Desktop\\Active-Face-Model\\photos\\' + str(i) + '.jpg'
         img = cv2.imread(path, cv2.IMREAD_COLOR)
         dets = detector(img, 1)
         for k, d in enumerate(dets):
@@ -103,8 +103,6 @@ def transfer(avg, U, X):
     registered_x = register_affine_face(avg, X)
     answer = np.linalg.lstsq(U, (avg.reshape((136, 1)) - registered_x.reshape((136, 1))), rcond=None)[0]
     transfered_face = avg.reshape((136, 1)) + U @ answer
-    print(transfered_face)
-    print(transfered_face.shape)
     plot_face(transfered_face.reshape((68, 2)), )
     plt.show()
 
@@ -112,18 +110,21 @@ def transfer(avg, U, X):
 face_list = collect_faces()
 for i in range(5):
     plot_face(face_list[i], color='g')
+    plt.show()
 base_face = face_list[0]
 team_mate = face_list.pop()
+print(len(face_list))
 # averaging faces - affine register
 affine_registered_list = []
 affine_registered_list.append(base_face)
 for i in range(1, len(face_list)):
+    bf = base_face - np.mean(base_face, axis=0)
     face = face_list[i]
     t = register_affine_face(base_face, face)
     affine_registered_list.append(t)
-    plot_face(t, color='r')
-    plot_face(face, color='k')
-    plt.show()
+    # plot_face(t, color='r')
+    # plot_face(bf, color='k')
+    # plt.show()
 
 
 # averaging faces - similarity register
@@ -131,19 +132,21 @@ similarity_registered_list = []
 similarity_registered_list.append(base_face)
 
 for i in range(1, len(face_list)):
+    bf = base_face - np.mean(base_face, axis=0)
     face = face_list[i]
     t = registered_similarity_face(base_face, face)
     similarity_registered_list.append(t)
-    plot_face(t, color='b')
-    plot_face(face, color='k')
-    plt.show()
+    # plot_face(t, color='b')
+    # plot_face(bf, color='k')
+    # plt.show()
 
 avg = sum(similarity_registered_list) / len(similarity_registered_list)
 plot_face(avg, color='orange')
+plt.show()
 
 # TODO: Change k
 k = 16
 affine_avg = sum(affine_registered_list) / len(affine_registered_list)
 U, sigma, V = PCA(similarity_registered_list, avg, k)
-animate(U, sigma, avg, k)
+# animate(U, sigma, avg, k)
 transfer(affine_avg, U, team_mate)
